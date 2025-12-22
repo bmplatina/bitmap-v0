98f41ab9c00dd0a7f508588dbe838610d42b0749
bmplatina <kkgordon.com@gmail.com>
2025-12-22 23:54:45 +0900

    Standalone production build

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,8 @@ const nextConfig = {
 
   // Uncomment below if using code-server development environment
   // basePath: "/absproxy/3000",
-}
+
+  output: "standalone", // 이 한 줄이 서버 리소스를 획기적으로 줄여줍니다.
+};
 
 export default nextConfig;
