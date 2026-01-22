94ea4d7e1455843842ad505ae2b7d62c29fdeda5
bmplatina <kkgordon.com@gmail.com>
2026-01-22 22:39:50 +0900

    carousel api 연동됨

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -14,65 +14,66 @@ export default async function Home() {
   const t = await getTranslations("MainPage");
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-full p-6 text-center space-y-12">
+    <>
       <Suspense fallback={<TitleFallback />}>
         <AutoSliderCarousel />
       </Suspense>
+      <div className="flex flex-col items-center w-full p-6 pt-10 text-center space-y-12">
+        {/* 유튜브 영상 가로 스크롤 섹션 */}
+        <div className="w-full max-w-6xl">
+          <h2 className="text-2xl font-bold mb-4 text-left">{t("works")}</h2>
+          <ScrollArea type="always" scrollbars="horizontal">
+            <div className="flex gap-4 overflow-x-auto pb-4 snap-x scrollbar-hide">
+              {youtubeVideos.length > 0 ? (
+                youtubeVideos.slice(0, 10).map((video) => (
+                  <div
+                    key={video}
+                    className="flex-none w-[85vw] md:w-[300px] aspect-video bg-black rounded-lg overflow-hidden snap-center shadow-md"
+                  >
+                    <iframe
+                      width="100%"
+                      height="100%"
+                      src={`https://www.youtube.com/embed/${video}`}
+                      frameBorder="0"
+                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                      allowFullScreen
+                    />
+                  </div>
+                ))
+              ) : (
+                <p className="text-gray-500 w-full text-center py-10">
+                  {t("api-error")}
+                </p>
+              )}
+            </div>
+          </ScrollArea>
+        </div>
 
-      {/* 유튜브 영상 가로 스크롤 섹션 */}
-      <div className="w-full max-w-6xl">
-        <h2 className="text-2xl font-bold mb-4 text-left">{t("works")}</h2>
-        <ScrollArea type="always" scrollbars="horizontal">
-          <div className="flex gap-4 overflow-x-auto pb-4 snap-x scrollbar-hide">
-            {youtubeVideos.length > 0 ? (
-              youtubeVideos.slice(0, 10).map((video) => (
-                <div
-                  key={video}
-                  className="flex-none w-[85vw] md:w-[300px] aspect-video bg-black rounded-lg overflow-hidden snap-center shadow-md"
-                >
-                  <iframe
-                    width="100%"
-                    height="100%"
-                    src={`https://www.youtube.com/embed/${video}`}
-                    frameBorder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    allowFullScreen
+        <div className="w-full max-w-6xl">
+          <h2 className="text-2xl font-bold mb-4 text-left">{t("games")}</h2>
+          <ScrollArea type="always" scrollbars="horizontal">
+            <div className="flex gap-4 pb-4">
+              {games.length > 0 ? (
+                games.map((game) => (
+                  <GameRedirectButton
+                    key={game.gameId}
+                    disabled={false}
+                    gameId={game.gameId}
+                    gameImageURL={game.gameImageURL[1] || game.gameImageURL[0]}
+                    gameTitle={game.gameTitle}
+                    gameDeveloper={game.gameDeveloper}
                   />
-                </div>
-              ))
-            ) : (
-              <p className="text-gray-500 w-full text-center py-10">
-                {t("api-error")}
-              </p>
-            )}
-          </div>
-        </ScrollArea>
+                ))
+              ) : (
+                <p className="text-gray-500 w-full text-center py-10">
+                  {t("api-error")}
+                </p>
+              )}
+            </div>
+          </ScrollArea>
+        </div>
       </div>
-
-      <div className="w-full max-w-6xl">
-        <h2 className="text-2xl font-bold mb-4 text-left">{t("games")}</h2>
-        <ScrollArea type="always" scrollbars="horizontal">
-          <div className="flex gap-4 pb-4">
-            {games.length > 0 ? (
-              games.map((game) => (
-                <GameRedirectButton
-                  key={game.gameId}
-                  disabled={false}
-                  gameId={game.gameId}
-                  gameImageURL={game.gameImageURL[1] || game.gameImageURL[0]}
-                  gameTitle={game.gameTitle}
-                  gameDeveloper={game.gameDeveloper}
-                />
-              ))
-            ) : (
-              <p className="text-gray-500 w-full text-center py-10">
-                {t("api-error")}
-              </p>
-            )}
-          </div>
-        </ScrollArea>
-      </div>
-    </div>
+    </>
   );
 }
 
